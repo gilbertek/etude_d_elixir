d35b7654a96640d01a7d2fc3099f160ab3e6afaa
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-05-12 20:35:09 -0400

    moar

---
+++ superviseur/lib/superviseur.ex
@@ -0,0 +1,18 @@
+defmodule Superviseur do
+  @moduledoc """
+  Documentation for Superviseur.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Superviseur.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
