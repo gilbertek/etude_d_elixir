c0a2a93cf91307b33c29fc12e7326e844bf00fa6
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-07-02 21:24:18 -0400

    Up

--- discuss/mix.exs
+++ discuss/mix.exs
@@ -18,8 +18,16 @@ defmodule Discuss.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {Discuss, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+      applications: [
+                      :phoenix,
+                      :phoenix_pubsub,
+                      :phoenix_html,
+                      :cowboy,
+                      :logger,
+                      :gettext,
+                      :phoenix_ecto,
+                      :postgrex
+                    ]]
   end
 
   # Specifies which paths to compile per environment.
